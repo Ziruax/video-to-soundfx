72c258fcd4ae75f61133481b341d619133505ea5
Ziruax <120768025+Ziruax@users.noreply.github.com>
2025-03-10 19:52:38 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Permanent fix for MoviePy regex warnings
-find /usr/local/lib/python3.*/site-packages/moviepy/ -type f -name "*.py" -exec sed -i 's/ re.search('\''\\d/ re.search(r'\''\\d/g' {} +
-find /usr/local/lib/python3.*/site-packages/moviepy/ -type f -name "*.py" -exec sed -i "s/ re.search('\\d/ re.search(r'\\d/g" {} +
-find /usr/local/lib/python3.*/site-packages/moviepy/ -type f -name "*.py" -exec sed -i 's/ re.search('\''\\P/ re.search(r'\''\\P/g' {} +
+# Completely remove moviepy
+pip uninstall -y moviepy
+
+# Alternative video processing
+pip install imageio imageio-ffmpeg
